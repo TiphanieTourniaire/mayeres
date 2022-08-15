7f51bdd06aacc3f3852ca30af050bc3702139898
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2022-08-15 22:39:52 +0200

    test sendgrid

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,8 +1,8 @@
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    user_name: "apikey",
-    password: ENV['SENDGRID_API_KEY'],
-    domain: 'http://monts-et-legumes.fr',
+    user_name: ENV["tiphanietourniaire@hotmail.com"],
+    password: ENV['Tiphanie88!!sendgrid'],
+    domain: ENV['http://monts-et-legumes.fr'],
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
